8cdcd4ea3f5f7e9621860ed3c66b2605904f931b
s1ash <dimapetrov200567@gmail.com>
2025-07-01 18:20:15 +0300

    библиотченые функции

--- main.py
+++ main.py
@@ -10,8 +10,75 @@ import datetime
 from zeep import Client
 from zeep.transports import Transport
 import requests
+from typing import List, Optional
+from pydantic import BaseModel
+import logging
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+class CustomFormatter(logging.Formatter):
+    green = "\033[32m"
+    yellow = "\033[33m"
+    red = "\033[31m"
+    bold_red = "\033[38m"
+    reset = "\033[31m"
+    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+    datefmt = '%Y-%m-%d %H:%M:%S'
+
+    FORMATS = {
+        logging.DEBUG: yellow + format + reset,
+        logging.INFO: green + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt, datefmt='%Y-%m-%d %H:%M:%S')
+        return formatter.format(record)
+
+
+logger = logging.getLogger("Antiplagiat")
+logger.setLevel(level=logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+ch.setFormatter(CustomFormatter())
+logger.addHandler(ch)
+
+
+class Source(BaseModel):
+    hash: str
+    score_by_report: str
+    score_by_source: str
+    name: Optional[str]
+    author: Optional[str]
+    url: Optional[str]
+
+class Service(BaseModel):
+    service_name: str
+    originality: str
+    plagiarism: str
+    source: Optional[List]
+
+class Author(BaseModel):
+    surname: Optional[str]
+    othernames: Optional[str]
+    custom_id: Optional[str]
+
+class LoanBlock(BaseModel):
+    text: str
+    offset: int
+    length: int
+
+class SimpleCheckResult(BaseModel):
+    filename: str
+    plagiarism: str
+    services: List[Service]
+    author: Optional[Author]
+    loan_blocks: Optional[List[LoanBlock]]
+
 
 class AntiplagiatClient:
     def __init__(self, login,
@@ -45,6 +112,8 @@ class AntiplagiatClient:
         return data
 
 
+
+
 if __name__ == "__main__":
     client = AntiplagiatClient(
         login="testapi@antiplagiat.ru",
@@ -53,6 +122,6 @@ if __name__ == "__main__":
     )
 
     print("\n$______________________Меню______________________$")
-    print("1. Загрузить и индексировать")
-    print("2. Проверить на оригинальность")
+    print("1. Загрузить и индексировать документ")
+    print("2. Проверить на оригинальность и получить отчет")
     print("0. Выход")
