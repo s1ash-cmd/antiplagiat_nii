13f5f16d46ce278a3bc21320798e519e6922a8a2
s1ash <dimapetrov200567@gmail.com>
2025-07-02 16:49:40 +0300

    asd

--- main.py
+++ main.py
@@ -7,108 +7,31 @@ import io
 import base64
 import sys
 import datetime
-from zeep import Client
-from zeep.transports import Transport
+from suds.client import Client
 import requests
-
 
 from libs.schemas import SimpleCheckResult, Service, Source, Author, LoanBlock
 from libs.logger import logger
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-
-from typing import List, Optional
-from pydantic import BaseModel
-import logging
-
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-
-class CustomFormatter(logging.Formatter):
-    green = "\033[32m"
-    yellow = "\033[33m"
-    red = "\033[31m"
-    bold_red = "\033[38m"
-    reset = "\033[31m"
-    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
-    datefmt = '%Y-%m-%d %H:%M:%S'
-
-    FORMATS = {
-        logging.DEBUG: yellow + format + reset,
-        logging.INFO: green + format + reset,
-        logging.WARNING: yellow + format + reset,
-        logging.ERROR: red + format + reset,
-        logging.CRITICAL: bold_red + format + reset
-    }
-
-    def format(self, record):
-        log_fmt = self.FORMATS.get(record.levelno)
-        formatter = logging.Formatter(log_fmt, datefmt='%Y-%m-%d %H:%M:%S')
-        return formatter.format(record)
-
-
-logger = logging.getLogger("Antiplagiat")
-logger.setLevel(level=logging.INFO)
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
-ch.setFormatter(CustomFormatter())
-logger.addHandler(ch)
-
-
-class Source(BaseModel):
-    hash: str
-    score_by_report: str
-    score_by_source: str
-    name: Optional[str]
-    author: Optional[str]
-    url: Optional[str]
-
-class Service(BaseModel):
-    service_name: str
-    originality: str
-    plagiarism: str
-    source: Optional[List]
-
-class Author(BaseModel):
-    surname: Optional[str]
-    othernames: Optional[str]
-    custom_id: Optional[str]
-
-class LoanBlock(BaseModel):
-    text: str
-    offset: int
-    length: int
-
-class SimpleCheckResult(BaseModel):
-    filename: str
-    plagiarism: str
-    services: List[Service]
-    author: Optional[Author]
-    loan_blocks: Optional[List[LoanBlock]]
-
-
 class AntiplagiatClient:
     def __init__(self, login,
                  password,
                  company_name,
-                 apicorp_address="api.antiplagiat.ru:4959"):
+                 apicorp_address="api.antiplagiat.ru:4959",
+                 antiplagiat_uri="https://testapi.antiplagiat.ru"):
 
-        self.login = login
-        self.password = password
+        self.antiplagiat_uri = antiplagiat_uri
+        self.login = "testapi@antiplagiat.ru"
+        self.password = "testapi"
         self.company_name = company_name
         self.apicorp_address = apicorp_address
 
-        wsdl_url = f'https://{self.apicorp_address}/apiCorp/{self.company_name}?wsdl'
-
-        session = requests.Session()
-        session.verify = False
-        session.auth = (self.login, self.password)
-
-        transport = Transport(session=session)
-        self.client = Client(wsdl=wsdl_url, transport=transport)
+        self.client = Client(f'https://{self.apicorp_address}/apiCorp/{self.company_name}?singleWsdl',
+                             username=self.login,
+                             password=self.password)
         print("SOAP клиент создан")
-
 
     def _get_doc_data(self, filename: str, external_user_id: str):
         data = self.client.factory.create("DocData")
@@ -140,7 +63,6 @@ class AntiplagiatClient:
         arr.AuthorName.append(author)
 
         docatr.DocumentDescription.Authors = arr
-
         # Загрузка файла
         try:
             uploadResult = self.client.service.UploadDocument(data, docatr)
@@ -162,15 +84,12 @@ class AntiplagiatClient:
         return data
 
 
-
-
 if __name__ == "__main__":
     client = AntiplagiatClient(
         login="testapi@antiplagiat.ru",
         password="testapi",
         company_name="testapi"
     )
-
 
     while True:
 
@@ -197,9 +116,3 @@ if __name__ == "__main__":
 
         else:
             print("Неверный запрос. Введите номер пункта из меню.")
-
-    print("\n$______________________Меню______________________$")
-    print("1. Загрузить и индексировать документ")
-    print("2. Проверить на оригинальность и получить отчет")
-    print("0. Выход")
-
