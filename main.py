501de3e742ceced61c7d635f6d8e2b6c247c7f76
anneett <ann_oganesyan59@mail.ru>
2025-07-02 19:46:56 +0300

    меню

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import io
 import base64
 import sys
 import datetime
-from suds.client import Client
+from zeep import Client
+from zeep.transports import Transport
 import requests
 
 from libs.schemas import SimpleCheckResult, Service, Source, Author, LoanBlock
@@ -28,18 +29,25 @@ class AntiplagiatClient:
         self.company_name = company_name
         self.apicorp_address = apicorp_address
 
-        self.client = Client(f'https://{self.apicorp_address}/apiCorp/{self.company_name}?singleWsdl',
-                                         username=self.login,
-                                         password=self.password)
+        wsdl_url = f'https://{self.apicorp_address}/apiCorp/{self.company_name}?singleWsdl'
+
+        session = requests.Session()
+        session.verify = False
+        session.auth = (self.login, self.password)
+
+        transport = Transport(session=session)
+        self.client = Client(wsdl=wsdl_url, transport=transport)
+
+        self.factory = self.client.type_factory('ns0')
         print("SOAP клиент создан")
 
     def _get_doc_data(self, filename: str, external_user_id: str):
-        data = self.client.factory.create("DocData")
-        data.Data = base64.b64encode(open(filename, "rb").read()).decode()
-        data.FileName = os.path.splitext(filename)[0]
-        data.FileType = os.path.splitext(filename)[1]
-        data.ExternalUserID = external_user_id
-        return data
+        return self.factory.DocData(
+            Data=base64.b64encode(open(filename, "rb").read()).decode(),
+            FileName=os.path.splitext(filename)[0],
+            FileType=os.path.splitext(filename)[1],
+            ExternalUserID=external_user_id
+        )
 
     def add_to_index(self, filename: str, author_surname='',
                      author_other_names='',
@@ -72,16 +80,88 @@ class AntiplagiatClient:
 
         # Идентификатор документа. Если загружается не архив, то список загруженных документов будет состоять из одного элемента.
         id = uploadResult.Uploaded[0].Id
-        return id
+        print (id)
 
-    def get_doc_data(self, filename: str, external_user_id: str):
-        data = {
-            "Data": base64.b64encode(open(filename, "rb").read()).decode(),
-            "FileName": os.path.splitext(filename)[0],
-            "FileType": os.path.splitext(filename)[1],
-            "ExternalUserID": external_user_id
-        }
-        return data
+        self.client.service.CheckDocument(id)
+
+            # Получить текущий статус последней проверки
+        status = self.client.service.GetCheckStatus(id)
+
+        # Цикл ожидания окончания проверки
+        while status.Status == "InProgress":
+            time.sleep(status.EstimatedWaitTime * 0.1)
+            status = self.client.service.GetCheckStatus(id)
+
+        # Проверка закончилась не удачно.
+        if status.Status == "Failed":
+            logger.error(f"An error occurred while validating the document {filename}: {status.FailDetails}")
+
+        # Получить краткий отчет
+        report = self.client.service.GetReportView(id)
+
+        logger.info(f"Report Summary: {report.Summary.Score:.2f}%")
+        result = SimpleCheckResult(filename=os.path.basename(filename),
+                                   plagiarism=f'{report.Summary.Score:.2f}%',
+                                   services=[],
+                                   author=Author())
+
+        for checkService in report.CheckServiceResults:
+            # Информация по каждому поисковому модулю
+
+            service = Service(service_name=checkService.CheckServiceName,
+                              originality=f'{checkService.ScoreByReport.Legal:.2f}%',
+                              plagiarism=f'{checkService.ScoreByReport.Plagiarism:.2f}%',
+                              source=[])
+
+            logger.info(f"Check service: {checkService.CheckServiceName}, "
+                        f"Score.White={checkService.ScoreByReport.Legal:.2f}% "
+                        f"Score.Black={checkService.ScoreByReport.Plagiarism:.2f}%")
+            if not hasattr(checkService, "Sources"):
+                result.services.append(service)
+                continue
+            for source in checkService.Sources:
+                _source = Source(hash=source.SrcHash,
+                                 score_by_report=f'{source.ScoreByReport:.2f}%',
+                                 score_by_source=f'{source.ScoreBySource:.2f}%',
+                                 name=source.Name,
+                                 author=source.Author,
+                                 url=source.Url)
+
+                service.source.append(_source)
+                # Информация по каждому найденному источнику
+                logger.info(
+                    f'\t{source.SrcHash}: Score={source.ScoreByReport:.2f}%({source.ScoreBySource:.2f}%), '
+                    f'Name="{source.Name}" Author="{source.Author}"'
+                    f' Url="{source.Url}"')
+
+                # Получить полный отчет
+            result.services.append(service)
+
+        options = self.client.factory.create("ReportViewOptions")
+        options.FullReport = True
+        options.NeedText = True
+        options.NeedStats = True
+        options.NeedAttributes = True
+        fullreport = self.client.service.GetReportView(id, options)
+
+        logger.info(f"Author Surname={fullreport.Attributes.DocumentDescription.Authors.AuthorName[0].Surname} "
+                    f"OtherNames={fullreport.Attributes.DocumentDescription.Authors.AuthorName[0].OtherNames} "
+                    f"CustomID={fullreport.Attributes.DocumentDescription.Authors.AuthorName[0].PersonIDs.CustomID}")
+
+        result.author.surname = fullreport.Attributes.DocumentDescription.Authors.AuthorName[0].Surname
+        result.author.othernames = fullreport.Attributes.DocumentDescription.Authors.AuthorName[0].OtherNames
+        result.author.custom_id = fullreport.Attributes.DocumentDescription.Authors.AuthorName[0].PersonIDs.CustomID
+
+        loan_blocks = []
+        if fullreport.Details.CiteBlocks:
+            for block in fullreport.Details.CiteBlocks:
+                loan_block = LoanBlock(text=fullreport.Details.Text[block.Offset:block.Offset + block.Length],
+                                       offset=block.Offset,
+                                       length=block.Length)
+                loan_blocks.append(loan_block)
+        result.loan_blocks = loan_blocks
+
+        return result.dict()
 
 
 if __name__ == "__main__":
@@ -92,13 +172,18 @@ if __name__ == "__main__":
     )
 
     while True:
-
         print("\n$____________________Меню____________________$")
         print("1. Загрузить и индексировать документ;")
         print("2. Проверить на оригинальность и получить отчет;")
         print("0. Выход.")
 
-        num = int(input("Введите пункт меню: "))
+        while True:
+            num = input("Введите пункт меню: ")
+            try:
+                num = int(num)
+                break
+            except ValueError:
+                print("Введите номер пункта из меню!")
 
         if num == 1:
             filename = input("Введите название файла для индексации: ")
@@ -112,7 +197,8 @@ if __name__ == "__main__":
             print("Отчет:")
 
         elif num == 0:
-            print("Выход.")
+            print("Выход...")
+            break
 
         else:
             print("Неверный запрос. Введите номер пункта из меню.")
